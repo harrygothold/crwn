f92afff30bd4492b5e6c24a2b1d3ec03b0a35ba3
Harry Gothold <harrygot@hotmail.com>
2019-07-19 18:44:00 +0100

    added basic header component and gitignore

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ShopPage from "./pages/shop";
+import Header from "./components/header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
